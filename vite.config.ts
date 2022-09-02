4f41dfd8837612c5c99b4541ac01981bbc4c441d
xYx <1yif@sina.com>
2022-09-02 17:52:16 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://****',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "src"),
+    }
+  }
+})
